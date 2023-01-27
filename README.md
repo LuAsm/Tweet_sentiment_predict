d43943b7210c91942eb021c36db3070e713ab45f
LuAsm <lukas.asmontas@gmail.com>
2023-01-27 18:44:16 +0200

    finish modelling ,api update readme start with sql

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# used-graphics-card-price-predict
+# Tweet_sentiment_predict
+
+* There were taken Tweets with Sentiment Dataset from https://www.kaggle.com/datasets/abhishek14398/10k-tweets-with-sentiments-dataset
+
+* In "notebooks"-"Tweet_modelling" you will find this dataset EDA and ML models which predict Sentiment from Tweet comment
+
+* Script app.py use fast api which with /predict path send input to the trained model and receive prediction.
+
+* in "notebooks"-"modelling" you will find my own scraped dataset EDA and ML models which should predict graphic card price.(not finished (updating))
--- src/app.py
+++ src/app.py
@@ -1,5 +1,5 @@
 from typing import Dict, List
-
+from src.db.connection import mydb
 import numpy as np
 import pandas as pd
 from fastapi import FastAPI
@@ -11,6 +11,21 @@ from src.utils.model_utils import load_model
 #Loading model
 tweet_model= load_model("https://www.dropbox.com/s/b7dztoz3iry3suw/tweet_model.pkl?dl=0")
 
+class DbConnection:
+    pass
+
+
+def write_to_db(log: str) -> bool:
+    db = DbConnection()
+    try:
+        db.write(log)
+        print("DB write ok")
+    except:
+        print("DB write failed")
+        return False
+    return True
+
+
 app = FastAPI()
 
 
@@ -20,9 +35,15 @@ def health() -> Dict[str, str]:
     return {"health": "alive"}
 
 
-@app.post("/predict_tweet_Sentiment")
+
+@app.post("/predict")
 def predict_tweet(inputs: List[tweet.Input]) -> Dict[str, List[Dict[str, float]]]:
+
+
     parsed_input = pd.DataFrame([i.dict() for i in inputs])
     outputs: NDArray[np.float32] = tweet_model.predict(parsed_input["Tweet"].values)
+    logs = {"path": "/predict", "input": input, "preds": outputs}
+    print (logs)
+    write_to_db(logs)
 
     return {"outputs": [{"Sentiment": i} for i in outputs.tolist()]}
